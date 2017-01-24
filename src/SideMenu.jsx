946c4eda834e47d4f9987f242d8b3134904390ec
Anzor Bashkhaz <anzorb@gmail.com>
2017-01-23 19:20:33 -0500

    first

---
+++ src/SideMenu.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SideMenu extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>HA!</div>
+    }
+
+};
+
+export default SideMenu;
